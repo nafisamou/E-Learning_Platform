ae0b2f184db424f3f30c71e97f9b81271ea89724
nafisamou <108416863+nafisamou@users.noreply.github.com>
2022-10-26 15:26:23 +0600

    card updated

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -8,13 +8,13 @@ import './Course.css'
 const Course = () => {
   const courses = useLoaderData();
   return (
-    <div className="grid lg:grid-cols-12 mt-12 m-10">
+    <div className="grid lg:grid-cols-12 mt-12 m-8">
       <div className=" col-span-3 ">
         <LeftSideNav></LeftSideNav>
       </div>
 
       <div className="col-span-9">
-        <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-10">
+        <div className="">
         {courses.map((course) => (
           <CourseCard key={course._id} course={course}></CourseCard>
         ))}
--- src/Pages/CourseCard/CourseCard.js
+++ src/Pages/CourseCard/CourseCard.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const CourseCard = ({course}) => {
-
-  const { title, img, ratings, details, _id, category,id } = course;
+const CourseCard = ({ course }) => {
+  const { title, img, ratings, details, _id, price } = course;
   return (
-    <div>
-      <div className="card w-96  h-[680px] bg-base-100 shadow-xl">
+    <div className="m-8">
+      <div className="card lg:card-side bg-base-100 shadow-xl">
         <figure>
-          <img className="w-fit h-60" src={img} alt="Shoes" />
+          <img className="w-64" src={img} alt="Album" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Name:{title}</h2>
+          <h2 className="card-title">Name: {title}</h2>
           <p>{details}</p>
-          <p >Ratings:<span className="ml-2">{ratings}</span>star</p>
+          <p className=""><strong>Price:</strong> {price}</p>
+          <p className="">
+            <strong className=" font-bold">Ratings:</strong><span className="ml-2">{ratings}</span> star
+          </p>
           <div className="card-actions justify-end">
-           <Link to={`/courses/${_id}`}> <button className="btn btn-primary">Course Details</button></Link>
-           
+            <Link to={`/courses/${_id}`}>
+              {" "}
+              <button className="btn btn-primary">Course Details</button>
+            </Link>
           </div>
         </div>
       </div>
